8dbe9a5b5ffacf0a5bc4f61e05d2898d10e6d632
Sujit Balasubramanian <sujitbalasubramanianr@gmail.com>
2023-05-28 10:23:23 +0530

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {HashRouter, Routes, Route} from "react-router-dom";
 import Login from './pages/Login'
 import AdminDashboard from './pages/AdminDashBoard'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<AdminDashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
